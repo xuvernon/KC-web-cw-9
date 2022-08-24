04b425f522e81f4665609e8c377db3b066b175e1
sarah almutari <sara0moha78@gmail.com>
2022-08-24 19:36:05 +0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // أنشئ المصفوفة تحت هذا الكومنت
-
+let orders = []
 
 function addOrder() {
   let name = document.getElementById("name").value;
@@ -8,16 +8,22 @@ function addOrder() {
   let order = document.getElementById("order").value;
   
   // أنشئ كائن newOrder
-  
+  let newOrder = 
+  {name,id,phone,order}
   // أدرج الكائن إلى المصفوفة
-  
+  orders.push(newOrder)
   // طبق الخطوة 7 تحت هذا الكومنت
-  
+  load()
 }
-
+let container = document.getElementById("container");
 function load() {
-  let container = document.getElementById("container");
-  container.innerHTML = "";
-  // طبّق الخطوة 6 تحت هذا الكومنت
 
+  // طبّق الخطوة 6 تحت هذا الكومنت
+  orders.forEach(x => {
+
+   container.innerHTML =  `<div><h1>${x.name} - ${x.id}</h1><h3>${x.order}</h3></div>`;
+});
+}
+function removeorder(){
+  container.remove();
 }
